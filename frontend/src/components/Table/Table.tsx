47d446fdf485905be5a39346008246f0ad5f8702
Artem Kodesnikov <artiguard@gmail.com>
2023-06-20 23:08:17 +0300

    some layout change

--- frontend/src/components/Table/Table.tsx
+++ frontend/src/components/Table/Table.tsx
@@ -18,21 +18,23 @@ type Props = {
 }
 
 export const IncomeTable: FC<Props> = ({ data, setIsLoading, fetchData }) => {
-  const incomePerHour = 75
-  const totalHours = data.reduce((acc, val) => acc += val.hours, 0)
-  const totalIncome = data.reduce((acc, val) => acc += (val.hours * incomePerHour), 0)
+  const incomePerHour = 75;
+  const totalHoursAdvance = data.filter((val) => +val.dayNumber <= 15).reduce((acc, val) => acc += val.hours, 0);
+  const totalHoursSalary = data.filter((val) => +val.dayNumber > 15).reduce((acc, val) => acc += val.hours, 0);
+  const totalAdvance = data.filter((val) => +val.dayNumber <= 15).reduce((acc, val) => acc += (val.hours * incomePerHour), 0);
+  const totalSalary = data.filter((val) => +val.dayNumber > 15).reduce((acc, val) => acc += (val.hours * incomePerHour), 0);
 
   const handleDelete = async (_id: string) => {
     setIsLoading(true);
     try {
       await deleteIncome(_id);
-      fetchData()
+      fetchData();
     } catch (error) {
-      console.log('Error:', error)
+      console.log('Error:', error);
     } finally {
-      setIsLoading(false)
+      setIsLoading(false);
     }
-  }
+  };
 
   return (
     <TableContainer component={Paper}>
@@ -46,49 +48,104 @@ export const IncomeTable: FC<Props> = ({ data, setIsLoading, fetchData }) => {
           </TableRow>
         </TableHead>
         <TableBody>
-          {data.map((row) => (
-            <TableRow
-              key={row._id}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-            >
-              <TableCell component="th" scope="row">
-                {row.dayNumber} {row.month} - {row.day}
-              </TableCell>
-              <TableCell align="right">{row.hours}</TableCell>
-              <TableCell align="right">{+row.dayNumber <= 15 ? 'Advance' : 'Salary'}</TableCell>
-              <TableCell align="right">
-                <Box sx={{
-                  display: 'flex',
-                  flexDirection: 'row',
-                  alignItems: 'flex-end',
-                  justifyContent: 'flex-end'
-                }}>
-                  {row.hours * incomePerHour}
-                  <Icon
-                    sx={{
-                      '&:hover': {
-                        color: 'red',
-                        cursor: 'pointer'
-                      }
-                    }}
-                    onClick={() => handleDelete(row._id)}
-                  >
-                    <DeleteIcon fontSize='medium' />
-                  </Icon>
-                </Box>
-              </TableCell>
-            </TableRow>
-          ))}
+          {data.map((row) => {
+            if (+row.dayNumber <= 15) {
+              return (
+                <TableRow
+                  key={row._id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.dayNumber} {row.month} - {row.day}
+                  </TableCell>
+                  <TableCell align="right">{row.hours}</TableCell>
+                  <TableCell align="right">{+row.dayNumber <= 15 ? 'Advance' : 'Salary'}</TableCell>
+                  <TableCell align="right">
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'flex-end',
+                        justifyContent: 'flex-end'
+                      }}
+                    >
+                      {row.hours * incomePerHour}
+                      <Icon
+                        sx={{
+                          '&:hover': {
+                            color: 'red',
+                            cursor: 'pointer'
+                          }
+                        }}
+                        onClick={() => handleDelete(row._id)}
+                      >
+                        <DeleteIcon fontSize="medium" />
+                      </Icon>
+                    </Box>
+                  </TableCell>
+                </TableRow>
+              );
+            }
+            return null;
+          })}
+          <TableRow sx={{ backgroundColor: '#2196f3' }}>
+            <TableCell sx={{ fontWeight: 500 }} component="th" scope="row">
+              Total advance (22: 1 - 15)
+            </TableCell>
+            <TableCell sx={{ fontWeight: 600 }} align="right">{totalHoursAdvance}</TableCell>
+            <TableCell align="right"></TableCell>
+            <TableCell sx={{ fontWeight: 600 }} align="right">{totalAdvance}</TableCell>
+          </TableRow>
+          {data.map((row) => {
+            if (+row.dayNumber > 15) {
+              return (
+                <TableRow
+                  key={row._id}
+                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {row.dayNumber} {row.month} - {row.day}
+                  </TableCell>
+                  <TableCell align="right">{row.hours}</TableCell>
+                  <TableCell align="right">{+row.dayNumber <= 15 ? 'Advance' : 'Salary'}</TableCell>
+                  <TableCell align="right">
+                    <Box
+                      sx={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        alignItems: 'flex-end',
+                        justifyContent: 'flex-end'
+                      }}
+                    >
+                      {row.hours * incomePerHour}
+                      <Icon
+                        sx={{
+                          '&:hover': {
+                            color: 'red',
+                            cursor: 'pointer'
+                          }
+                        }}
+                        onClick={() => handleDelete(row._id)}
+                      >
+                        <DeleteIcon fontSize="medium" />
+                      </Icon>
+                    </Box>
+                  </TableCell>
+                </TableRow>
+              );
+            }
+            return null;
+          })}
           <TableRow sx={{ backgroundColor: '#019a01' }}>
             <TableCell sx={{ fontWeight: 500 }} component="th" scope="row">
-              Total
+              Total salary (7: 16 - end of month)
             </TableCell>
-            <TableCell sx={{ fontWeight: 600 }} align="right">{totalHours}</TableCell>
+            <TableCell sx={{ fontWeight: 600 }} align="right">{totalHoursSalary}</TableCell>
             <TableCell align="right"></TableCell>
-            <TableCell sx={{ fontWeight: 600 }} align="right">{totalIncome}</TableCell>
+            <TableCell sx={{ fontWeight: 600 }} align="right">{totalSalary}</TableCell>
           </TableRow>
         </TableBody>
       </Table>
-    </TableContainer >
+    </TableContainer>
   );
-}
+};
