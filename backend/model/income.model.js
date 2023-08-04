e2b1d3a68f55d347dd97e9cc3901885a8bb56ac3
Artem Kodesnikov <artiguard@gmail.com>
2023-08-04 17:46:57 +0300

    add value to model

--- backend/model/income.model.js
+++ backend/model/income.model.js
@@ -1,10 +1,11 @@
-import { Schema, model, Types } from 'mongoose';
+import { Schema, model } from 'mongoose';
 
 const Income = new Schema({
   day: { type: String, required: true },
   dayNumber: { type: String, required: true },
   month: { type: String, required: true },
   hours: { type: Number, required: true },
+  comment: { type: String, required: false }
 });
 
 export default model('Income', Income);
